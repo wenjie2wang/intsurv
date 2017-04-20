5a746a3bda709dab903db1acb14910b9cc4e2798
Wenjie Wang <wenjie.2.wang@uconn.edu>
2017-04-19 20:16:20 -0400

    add simple package documentation

---
+++ R/intsurv.R
@@ -0,0 +1,16 @@
+##' Integrative Survival Modeling
+##'
+##' The package \pkg{intsurv} mainly provides function fitting extended Cox
+##' proportional hazard model for uncertain survival data due to imperfect data
+##' integration.
+##'
+##' @references
+##'
+##' Wang, W., Chen, K., & Yan, J. (2017+).  Extended Cox Model by ECM Algorithm
+##' for Uncertain Survival Records Due to Imperfect Data Integration. (working
+##' in progress)
+##'
+##' @importFrom methods setClass setGeneric setMethod new validObject
+##' @docType package
+##' @name intsurv-package
+NULL
