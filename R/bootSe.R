9f475a55883ef944e02d3f58e0e910391938b9df
Wenjie Wang <wenjie.2.wang@uconn.edu>
2017-04-19 22:16:35 -0400

    add function documentation for bootSe

--- R/bootSe.R
+++ R/bootSe.R
@@ -1,51 +1,139 @@
-## estimate se by bootstrapping methods
-bootSe <- function(obj, numBoot = 200, control = list(), ...)
+##' Standard Error Estimates through Bootstrapping Methods
+##'
+##' This function addes or updates standard error (SE) estimates through
+##' bootstrap method for \code{\link{intCox-class}} object by default.  Three
+##' different methods are available for computing SE from bootstrap samples
+##' through argument \code{se}.
+##'
+##' Given the fact that the bootstrap method is computional intensive, the
+##' function can return the coefficient estimates from one bootstrap sample when
+##' \code{control = list(estOnly = TRUE)} is specified, which can used in
+##' parallel computing or high performance computing (HPC) cluster. Then the SE
+##' estimates can be easily computed based on estimates from bootstrap samples.
+##'
+##' The available elements of argument \code{start} are the same with those of
+##' argument \code{start} in function \code{\link{coxphx}} except that the
+##' \code{piVec} is not available since its length may vary in different
+##' bootstrap samples.
+##'
+##' @usage
+##' bootSe(object, numBoot = 50, se = c("mad", "inter-quantile", "sd"),
+##'        control = list(), ...)
+##'
+##' @param object \code{\link{intCox-class}} object.
+##' @param numBoot A positive integer specifying number of bootstrap samples
+##'     used for SE estimates.  A large number, such as 200, is often needed for
+##'     a more reliable estimation in practice.
+##' @param se A character value specifying the way computing SE from bootstrap
+##'     samples. The default method is based on median absolute deviation and
+##'     the second method is based on inter-quantile, both of which are based on
+##'     normality of the bootstrap estimates and provids robust estimates for
+##'     SE. The third method estimates SE by the standard deviation of the
+##'     bootstrap estimates.
+##' @param start An optional list of starting values for the parameters to be
+##'     estimated in the model.  See more in Section details.
+##' @param control A optional list that controls the model fitting process for
+##'     bootstrap samples, or specifys the function to return coefficient
+##'     estimates from the bootstrap samples. See the available options in
+##'     Section Details.
+##'
+##' @return \code{\link{intCox-class}} object by default or a numeric matrix of
+##'     coefficient estimates from each bootstrap sample.
+##'
+##' @references
+##'
+##' Wang, W., Chen, K., & Yan, J. (2017+).  Extended Cox Model by ECM Algorithm
+##' for Uncertain Survival Records Due to Imperfect Data Integration. (working
+##' in progress)
+##'
+##' @examples
+##' ## See examples given in function 'coxphx' by '?coxphx'.
+##' @seealso
+##' \code{\link{coxphx}} for fitting extended Cox model for uncertain records.
+##' @importFrom stats quantile pnorm
+##' @export
+bootSe <- function(object, numBoot = 50, se = c("mad", "inter-quantile", "sd"),
+                   start = list(), control = list(), ...)
 {
-    fm <- obj@formula
-    cal <- obj@call
-    censorRate0 <- obj@start$censorRate0
-    control <- do.call(bootSe_control,
-                       c(control, list(censorRate0_ = censorRate0)))
-    start0 <- list(beta = obj@start$beta, censorRate = control$startGrid)
-    cal$start <- quote(start0)
-    cal$control <- obj@control
-    cal$control$noSE_ <- TRUE
+    if (! inherits(object, "intCox"))
+        stop("The 'object' has to be an 'intCox' class object.")
+    se <- match.arg(se)
+    fm <- object@formula
+    cal <- object@call
+    ## update start list
+    start <- do.call(bootSe_start, c(start, list(start0 = object@start)))
+    cal$start <- quote(start)
+    ## update local control list
+    control <- do.call(bootSe_control, control)
+    ## add noSE = TRUE to the original control list
+    cal$control <- object@control
+    cal$control$noSE <- TRUE
     cal$data <- quote(bootDat)
-    dat <- obj@data
+    dat <- object@data
     idName <- as.character(fm[[2L]][[2L]])
     timeName <- as.character(fm[[2L]][[3L]])
     dat <- dat[order(dat[, idName], dat[, timeName]), ]
-    idTab <- table(dat$ID)
-    uid <- unique(dat$ID)
+    idTab <- table(dat[, idName])
+    uid <- unique(dat[, idName])
     nSub <- length(uid)
     estMat <- replicate(numBoot, {
         sID <- sort(sample(uid, size = nSub, replace = TRUE))
         tmpDat <- data.frame(ID = sID)
+        colnames(tmpDat) <- idName
         repNum <- idTab[match(as.character(sID), names(idTab))]
-        bootDat <- merge(tmpDat, dat, by = "ID")
-        bootDat$ID <- rep(seq_along(uid), repNum)
+        bootDat <- merge(tmpDat, dat, by = idName)
+        bootDat[[idName]] <- rep(seq_along(uid), repNum)
         res <- eval(cal)
         as.numeric(res@estimates$beta[, "coef"])
     })
-    if (control$estOnly)
-        return(as.numeric(estMat))
-    obj@estimates$beta[, "se(coef)"] <- apply(estMat, 1L, sd)
-    tmp <- obj@estimates$beta[, "z"] <- obj@estimates$beta[, "coef"] /
-        obj@estimates$beta[, "se(coef)"]
-    obj@estimates$beta[, "Pr(>|z|)"] <- 2 * stats::pnorm(- abs(tmp))
-    obj
+    if (control$estOnly) {
+        nBeta <- NROW(estMat)
+        estMat <- t(estMat)
+        colnames(estMat) <- paste0("b", seq_len(nBeta))
+        return(estMat)
+    }
+    if (identical(se, "mad")) {
+        object@estimates$beta[, "se(coef)"] <- apply(estMat, 1L, function(a) {
+            median(abs(a - median(a))) * 1.4826
+        })
+    } else if (identical(se, "inter-quantile")) {
+        object@estimates$beta[, "se(coef)"] <- apply(estMat, 1L, function(a) {
+            diff(stats::quantile(a, probs = c(0.25, 0.75))) /
+                (qnorm(0.75) - qnorm(0.25))
+        })
+    } else (identical(se, "sd"))
+        object@estimates$beta[, "se(coef)"] <- apply(estMat, 1L, sd)
+
+    tmp <- object@estimates$beta[, "z"] <- object@estimates$beta[, "coef"] /
+        object@estimates$beta[, "se(coef)"]
+    object@estimates$beta[, "Pr(>|z|)"] <- 2 * stats::pnorm(- abs(tmp))
+    object
 }
 
 
-### internal functions
-bootSe_control <- function(startGrid, fixStart = FALSE, estOnly = FALSE,
-                           ..., censorRate0_)
+### internal functions =========================================================
+bootSe_start <- function(beta0, censorRate, multiStart = FALSE, ...,
+                         start0)
 {
-    startGrid <- if (fixStart) {
-                     censorRate0_
-                 } else if (missing(startGrid)){
-                     seq.int(max(0, censorRate0_ - 0.2),
-                             min(1, censorRate0_ + 0.2), 0.05)
-                 }
-    list(startGrid = startGrid, estOnly = estOnly)
+    censorRate0 <- start0$censorRate0
+    if (missing(beta0))
+        beta <- start0$beta
+    if (missing(censorRate)) {
+        censorRate <- if (! multiStart) {
+                          censorRate0
+                      } else if (missing(startGrid)){
+                          seq.int(max(0, censorRate0 - 0.2),
+                                  min(1, censorRate0 + 0.2), 0.05)
+                      }
+    }
+    list(beta = beta, censorRate = censorRate,
+         censorRate0 = censorRate0)
+}
+
+
+bootSe_control <- function(estOnly = FALSE, ...)
+{
+    if (! is.logical(estOnly))
+        stop("Argument 'estOnly' has to be a logical value.")
+    list(estOnly = estOnly)
 }
