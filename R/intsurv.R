f80be83fa0fd22891f75f211a25af8436c92ce9d
Wenjie Wang <wjwang.stat@gmail.com>
2018-09-01 16:01:43 -0400

    minor updates on copyright header

--- R/intsurv.R
+++ R/intsurv.R
@@ -1,22 +1,19 @@
-################################################################################
 ##
-##   R package intsurv by Wenjie Wang, Kun Chen and Jun Yan
-##   Copyright (C) 2017-2018
+## intsurv: Integrative Survival Models
+## Copyright (C) 2017-2018  Wenjie Wang <wjwang.stat@gmail.com>
 ##
-##   This file is part of the R package intsurv.
+## This file is part of the R package intsurv.
 ##
-##   The R package intsurv is free software: You can redistribute it and/or
-##   modify it under the terms of the GNU General Public License as published
-##   by the Free Software Foundation, either version 3 of the License, or
-##   any later version (at your option). See the GNU General Public License
-##   at <http://www.gnu.org/licenses/> for details.
+## The R package intsurv is free software: You can redistribute it and/or
+## modify it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or any later
+## version (at your option). See the GNU General Public License at
+## <https://www.gnu.org/licenses/> for details.
 ##
-##   The R package intsurv is distributed in the hope that it will be useful,
-##   but WITHOUT ANY WARRANTY without even the implied warranty of
-##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+## The R package intsurv is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 ##
-################################################################################
-
 
 ##' Integrative Survival Modeling
 ##'
