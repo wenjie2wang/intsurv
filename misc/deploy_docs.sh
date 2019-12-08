33eb37fc4ea74da2edada6dba61c12df639acdd3
Wenjie Wang <wjwang.stat@gmail.com>
2019-12-07 22:58:40 -0500

    tweak script for pkg page deploy

--- misc/deploy_docs.sh
+++ misc/deploy_docs.sh
@@ -4,11 +4,13 @@ set -e
 
 ## run on wenjie's droplets
 build_dir=$(pwd)
-cd $HOME/wenjie/wenjie-stat.me/
+target_dir=$HOME/wenjie/wenjie-stat.me/static/intsurv
+cd $HOME/wenjie/wenjie-stat.me
 git checkout -f
 git checkout master
 git pull origin master
-cp -r $build_dir/docs/* $HOME/wenjie/wenjie-stat.me/static/intsurv/
+mkdir -p $target_dir
+cp -r $build_dir/docs/* $target_dir
 tmp_log=.git_status.log
 git status > $tmp_log
 if egrep -q "modified:[ ]+docs/" $tmp_log
