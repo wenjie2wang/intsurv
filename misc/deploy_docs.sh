998f138ca6e8613f67df7a560cd443f25ce32da5
Wenjie Wang <wjwang.stat@gmail.com>
2019-12-08 23:22:49 -0500

    tweak the script for page deployment

--- misc/deploy_docs.sh
+++ misc/deploy_docs.sh
@@ -3,20 +3,21 @@
 set -e
 
 ## run on wenjie's droplets
+pkg=$(grep "Package" DESCRIPTION | awk '{print $NF}')
 build_dir=$(pwd)
-target_dir=$HOME/wenjie/wenjie-stat.me/static/intsurv
+target_dir=$HOME/wenjie/wenjie-stat.me/static/$pkg
 tmp_log=.git_status.log
-cd $HOME/wenjie/wenjie-stat.me
+cd $HOME/wenjie/wenjie-stat.me/
 git checkout -f
 git checkout master
 git pull origin master
 mkdir -p $target_dir
 cp -r $build_dir/docs/* $target_dir
 git status > $tmp_log
-if egrep -q "modified:[ ]+static/intsurv" $tmp_log
+if egrep -q "modified:[ ]+static/$pkg/" $tmp_log
 then
-    git add -u static/intsurv/
-    git commit -m "deploy intsurv $CI_COMMIT_SHORT_SHA by gitlab-runner"
+    git add -u static/$pkg/
+    git commit -m "deploy $pkg $CI_COMMIT_SHORT_SHA by gitlab-runner"
     git push origin master
 else
     printf "The docs was not updated.\n"
