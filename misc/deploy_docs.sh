b71c88f2711a0610d8f3f53833bcd34854bfdda6
Wenjie Wang <wjwang.stat@gmail.com>
2019-12-08 22:30:35 -0500

    fix docs deployment

--- misc/deploy_docs.sh
+++ misc/deploy_docs.sh
@@ -5,17 +5,17 @@ set -e
 ## run on wenjie's droplets
 build_dir=$(pwd)
 target_dir=$HOME/wenjie/wenjie-stat.me/static/intsurv
+tmp_log=.git_status.log
 cd $HOME/wenjie/wenjie-stat.me
 git checkout -f
 git checkout master
 git pull origin master
 mkdir -p $target_dir
 cp -r $build_dir/docs/* $target_dir
-tmp_log=.git_status.log
 git status > $tmp_log
-if egrep -q "modified:[ ]+docs/" $tmp_log
+if egrep -q "modified:[ ]+static/intsurv" $tmp_log
 then
-    git add -u static/reda/
+    git add -u static/intsurv/
     git commit -m "deploy intsurv $CI_COMMIT_SHORT_SHA by gitlab-runner"
     git push origin master
 else
