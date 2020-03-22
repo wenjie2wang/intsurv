41cc8bc07792ff13b887118ce46ca9a8a13f82d9
Wenjie Wang <wjwang.stat@gmail.com>
2020-03-22 13:13:34 -0400

    let largest event time stay in training set

--- inst/include/intsurv/cross-validation.h
+++ inst/include/intsurv/cross-validation.h
@@ -17,6 +17,23 @@ namespace Intsurv {
         // default constructor
         CrossValidation();
 
+        // major constructor
+        CrossValidation(const unsigned long n_obs,
+                        const unsigned long n_folds) :
+            n_obs_ { n_obs },
+            n_folds_ { n_folds }
+        {
+            this->test_index = get_cv_test_index(n_obs_, n_folds_);
+            arma::uvec all_index {
+                arma::regspace<arma::uvec>(0, n_obs - 1)
+            };
+            for (size_t i {0}; i < n_folds_; ++i) {
+                this->train_index.push_back(
+                    vec_diff(all_index, this->test_index.at(i))
+                    );
+            }
+        }
+
         // explicit constructor
         explicit CrossValidation(const unsigned long n_obs) :
             n_obs_ { n_obs }
@@ -24,13 +41,21 @@ namespace Intsurv {
             CrossValidation(n_obs_, 10);
         }
 
-        // constructor
+        // a special constructor
         CrossValidation(const unsigned long n_obs,
-                        const unsigned long n_folds) :
+                        const unsigned long n_folds,
+                        // keep static index always in the training set
+                        const arma::uvec static_train_index) :
             n_obs_ { n_obs },
             n_folds_ { n_folds }
         {
             this->test_index = get_cv_test_index(n_obs_, n_folds_);
+            // remove static train index from test index
+            for (size_t i {0}; i < n_folds_; ++i) {
+                this->test_index.at(i) = vec_diff(
+                    this->test_index.at(i), static_train_index
+                    );
+            }
             arma::uvec all_index {
                 arma::regspace<arma::uvec>(0, n_obs - 1)
             };
--- inst/include/intsurv/cv_coxph_cure.h
+++ inst/include/intsurv/cv_coxph_cure.h
@@ -32,13 +32,9 @@ namespace Intsurv {
         const unsigned int& verbose = 0
         )
     {
-        // stratified sampling
+        // stratify
         const arma::uvec case1_ind = arma::find(event > 0);
         const arma::uvec case2_ind = arma::find(event < 1);
-        const unsigned long n_case1 { case1_ind.n_elem };
-        const unsigned long n_case2 { case2_ind.n_elem };
-        CrossValidation cv_obj_case1 { n_case1, n_folds };
-        CrossValidation cv_obj_case2 { n_case2, n_folds };
         const arma::vec time_case1 { time.elem(case1_ind) };
         const arma::vec time_case2 { time.elem(case2_ind) };
         const arma::vec event_case1 { event.elem(case1_ind) };
@@ -47,6 +43,13 @@ namespace Intsurv {
         const arma::mat cox_x_case2 { cox_x.rows(case2_ind) };
         const arma::mat cure_x_case1 { cure_x.rows(case1_ind) };
         const arma::mat cure_x_case2 { cure_x.rows(case2_ind) };
+        // get the index of the largest event time
+        const arma::uvec which_time_max { time_case1.index_max() };
+        // cross-validation
+        const unsigned long n_case1 { case1_ind.n_elem };
+        const unsigned long n_case2 { case2_ind.n_elem };
+        CrossValidation cv_obj_case1 { n_case1, n_folds, which_time_max };
+        CrossValidation cv_obj_case2 { n_case2, n_folds };
 
         arma::vec cv_vec { arma::zeros(n_folds) };
         for (size_t i {0}; i < n_folds; ++i) {
@@ -119,6 +122,134 @@ namespace Intsurv {
         }
         return cv_vec;
     }
+
+    // regularized Cox cure model
+    arma::vec cv_coxph_cure_reg(
+        const arma::vec& time,
+        const arma::vec& event,
+        const arma::mat& cox_x,
+        const arma::mat& cure_x,
+        const bool cure_intercept = true,
+        const unsigned long n_folds = 10,
+        const double& cox_l1_lambda = 0,
+        const double& cox_l2_lambda = 0,
+        const arma::vec& cox_l1_penalty_factor = 0,
+        const double& cure_l1_lambda = 0,
+        const double& cure_l2_lambda = 0,
+        const arma::vec& cure_l1_penalty_factor = 0,
+        const arma::vec& cox_start = 0,
+        const arma::vec& cure_start = 0,
+        const bool& cox_standardize = true,
+        const bool& cure_standardize = true,
+        const unsigned int& em_max_iter = 1000,
+        const double& em_rel_tol = 1e-4,
+        const unsigned int& cox_mstep_max_iter = 200,
+        const double& cox_mstep_rel_tol = 1e-4,
+        const unsigned int& cure_mstep_max_iter = 200,
+        const double& cure_mstep_rel_tol = 1e-4,
+        const unsigned int& tail_completion = 1,
+        double tail_tau = -1,
+        const double& pmin = 1e-5,
+        const unsigned int& early_stop = 0,
+        const unsigned int& verbose = 0
+        )
+    {
+        // stratify
+        const arma::uvec case1_ind = arma::find(event > 0);
+        const arma::uvec case2_ind = arma::find(event < 1);
+        const arma::vec time_case1 { time.elem(case1_ind) };
+        const arma::vec time_case2 { time.elem(case2_ind) };
+        const arma::vec event_case1 { event.elem(case1_ind) };
+        const arma::vec event_case2 { event.elem(case2_ind) };
+        const arma::mat cox_x_case1 { cox_x.rows(case1_ind) };
+        const arma::mat cox_x_case2 { cox_x.rows(case2_ind) };
+        const arma::mat cure_x_case1 { cure_x.rows(case1_ind) };
+        const arma::mat cure_x_case2 { cure_x.rows(case2_ind) };
+        // get the index of the largest event time
+        const arma::uvec which_time_max { time_case1.index_max() };
+        // cross-validation
+        const unsigned long n_case1 { case1_ind.n_elem };
+        const unsigned long n_case2 { case2_ind.n_elem };
+        CrossValidation cv_obj_case1 { n_case1, n_folds, which_time_max };
+        CrossValidation cv_obj_case2 { n_case2, n_folds };
+
+        arma::vec cv_vec { arma::zeros(n_folds) };
+        for (size_t i {0}; i < n_folds; ++i) {
+            // training set
+            arma::vec train_time {
+                arma::join_vert(
+                    time_case1.elem(cv_obj_case1.train_index.at(i)),
+                    time_case2.elem(cv_obj_case2.train_index.at(i))
+                    )
+            };
+            arma::vec train_event {
+                arma::join_vert(
+                    event_case1.elem(cv_obj_case1.train_index.at(i)),
+                    event_case2.elem(cv_obj_case2.train_index.at(i))
+                    )
+            };
+            arma::mat train_cox_x {
+                arma::join_vert(
+                    cox_x_case1.elem(cv_obj_case1.train_index.at(i)),
+                    cox_x_case2.elem(cv_obj_case2.train_index.at(i))
+                    )
+            };
+            arma::mat train_cure_x {
+                arma::join_vert(
+                    cure_x_case1.elem(cv_obj_case1.train_index.at(i)),
+                    cure_x_case2.elem(cv_obj_case2.train_index.at(i))
+                    )
+            };
+            // testing set
+            arma::vec test_time {
+                arma::join_vert(
+                    time_case1.elem(cv_obj_case1.test_index.at(i)),
+                    time_case2.elem(cv_obj_case2.test_index.at(i))
+                    )
+            };
+            arma::vec test_event {
+                arma::join_vert(
+                    event_case1.elem(cv_obj_case1.test_index.at(i)),
+                    event_case2.elem(cv_obj_case2.test_index.at(i))
+                    )
+            };
+            arma::mat test_cox_x {
+                arma::join_vert(
+                    cox_x_case1.elem(cv_obj_case1.test_index.at(i)),
+                    cox_x_case2.elem(cv_obj_case2.test_index.at(i))
+                    )
+            };
+            arma::mat test_cure_x {
+                arma::join_vert(
+                    cure_x_case1.elem(cv_obj_case1.test_index.at(i)),
+                    cure_x_case2.elem(cv_obj_case2.test_index.at(i))
+                    )
+            };
+            // define object
+            CoxphCure cc_obj {
+                train_time, train_event, train_cox_x, train_cure_x,
+                cure_intercept, cox_standardize, cure_standardize
+            };
+            // model-fitting
+            cc_obj.regularized_fit(
+                cox_l1_lambda, cox_l2_lambda,
+                cure_l1_lambda, cure_l2_lambda,
+                cox_l1_penalty_factor, cure_l1_penalty_factor,
+                cox_start, cure_start,
+                em_max_iter, em_rel_tol,
+                cox_mstep_max_iter, cox_mstep_rel_tol,
+                cure_mstep_max_iter, cure_mstep_rel_tol,
+                tail_completion, tail_tau,
+                pmin, early_stop, verbose
+                );
+            // compute observed log-likelihood function for the test data
+            cv_vec(i) = cc_obj.obs_log_likelihood(
+                test_time, test_event, test_cox_x, test_cure_x, pmin
+                );
+        }
+        return cv_vec;
+    }
+
 
 }  // Intsurv
 
