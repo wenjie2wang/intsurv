30522f6d8507b339124c45542366e35bedef3f6e
Wenjie Wang <wang@wwenjie.org>
2022-05-10 10:34:58 -0400

    Add Control

---
+++ inst/include/intsurv/control.h
@@ -0,0 +1,122 @@
+#ifndef INTSURV_CONTROL_H
+#define INTSURV_CONTROL_H
+
+#include <RcppArmadillo.h>
+#include <stdexcept>
+#include "utils.h"
+
+namespace Intsurv {
+
+    class Control
+    {
+    public:
+        // common
+        unsigned int max_iter_ { 1000 };
+        double epsilon_ { 1e-4 };
+        bool standardize_ { true };
+        arma::vec start_ { arma::vec() };
+        arma::vec offset_ { arma::vec() };
+        unsigned int verbose_ { 0 };
+
+        // for logistic models
+        bool intercept_ { true };
+        double pmin_ { 1e-5 };
+
+        // for cure rate models
+        unsigned int tail_completion_ { 1 };
+        double tail_tau_ { - 1.0 };
+
+        // for elastic-net models
+        // common
+        arma::vec penalty_factor_ { arma::vec() };
+        bool varying_active_ { true };
+        // single lambda's
+        double l1_lambda_;
+        double l2_lambda_;
+        // solution paths
+        unsigned int nlambda_;
+        double lambda_min_ratio_;
+        double alpha_;
+        arma::vec lambda_ { arma::vec() };
+
+        // default constructor
+        Control() {}
+
+        Control(const unsigned int max_iter,
+                const double epsilon)
+        {
+            if (is_lt(epsilon, 0.0)) {
+                throw std::range_error("The 'epsilon' cannot be negative.");
+            }
+            max_iter_ = max_iter;
+            epsilon_ = epsilon;
+        }
+
+        // methods to set values for different models
+        Control* logistic(const bool intercept = true,
+                          const double pmin = 1e-5)
+        {
+            if (is_gt(pmin, 0.01) || is_le(pmin, 0.0)) {
+                throw std::range_error(
+                    "The 'pmin' must be between 0 and 0.01.");
+            }
+            intercept_ = intercept;
+            pmin_ = pmin;
+            return this;
+        }
+        Control* cure(const unsigned int tail_completion = 1,
+                      const double tail_tau = -1.0)
+        {
+            tail_completion_ = tail_completion;
+            tail_tau_ = tail_tau;
+            return this;
+        }
+        Control* net(const arma::vec& penalty_factor = arma::vec(),
+                     const bool varying_active = true)
+        {
+            if (! penalty_factor.is_empty() &&
+                arma::any(penalty_factor < 0.0)) {
+                throw std::range_error(
+                    "The 'penalty_factor' cannot be negative.");
+            }
+            penalty_factor_ = penalty_factor;
+            varying_active_ = varying_active;
+            return this;
+        }
+        Control* net_fit(const double l1_lambda,
+                         const double l2_lambda)
+        {
+            if (is_lt(l1_lambda, 0.0) || is_lt(l2_lambda, 0.0)) {
+                throw std::range_error("The 'lambda' cannot be negative.");
+            }
+            l1_lambda_ = l1_lambda;
+            l2_lambda_ = l2_lambda;
+            return this;
+        }
+        Control* net_path(const unsigned int nlambda,
+                          const double lambda_min_ratio,
+                          const double alpha = 1.0,
+                          const arma::vec lambda = arma::vec())
+        {
+            if (is_gt(lambda_min_ratio, 1.0) || is_le(lambda_min_ratio, 0.0)) {
+                throw std::range_error(
+                    "The 'lambda_min_ratio' must be between 0 and 1.");
+            }
+            if (is_gt(alpha, 1.0) || is_le(alpha, 0.0)) {
+                throw std::range_error(
+                    "The 'alpha' must be between 0 and 1.");
+            }
+            if (! lambda.is_empty() && arma::any(lambda < 0.0)) {
+                throw std::range_error("The 'lambda' cannot be negative.");
+            }
+            lambda_min_ratio_ = lambda_min_ratio;
+            nlambda_ = nlambda;
+            alpha_ = alpha;
+            lambda_ = lambda;
+            return this;
+        }
+    };
+
+}
+
+#endif /* INTSURV_CONTROL_H */
